646415ea0c519f5781e51932c979cd1af34cdb37
Jackson-p <1216884492@qq.com>
2020-05-09 00:48:52 +0800

    feat: issues爬虫写法优化

--- issueCrawler.py
+++ issueCrawler.py
@@ -1,32 +1,33 @@
 # -*- coding:utf-8 -*-
 
 '''
-爬取某个用户的所有issue主要是适合保存备份
+爬取某个用户的所有issue主要是适合保存备份, 注意一次最多只能请求30条数据，所以要先拿到页数，
+然后再分页请求
 '''
 
-
-import urllib.request as urll
-import json
 import re
 import time
+import pip._vendor.requests as requests
 
 print('输入github用户名')
 user_name = input()
 
 
-center_url = "https://api.github.com/search/issues?q=+state:open+repo:" + user_name + "/" + user_name + ".github.io"
-response = urll.urlopen(center_url)
-header = str(response.headers)
-rels = re.findall(r'page=(\d+)',header)
-page_number = int(rels[1])
+page_url = "https://api.github.com/search/issues?q=+state:open+repo:" + user_name + "/" + user_name + ".github.io"
+page_res = requests.get(page_url)
+page_header = str(page_res.headers)
+page_rels = re.findall(r'page=(\d+)', page_header)
+page_number = int(page_rels[1])
+
+
+
 
 def getIssues(page):
     url = "https://api.github.com/search/issues?q=+state:open+repo:" + user_name + "/" + user_name + ".github.io&sort=created&page=" + str(page)
-    issues = urll.urlopen(url).read().decode('utf-8')
-    issues = json.loads(issues)['items']
+    issues = requests.get(url).json().get('items')
     for issue in issues:
         title = issue['title']
-        label = issue['labels'][0]['name'] + '~~'  # be careful diffrom js
+        label = (issue['labels'] and issue['labels'][0]['name']) + '~~'  # be careful diffrom js
         body = issue['body']
         name = label + title + '.md'
         with open(name, 'a') as f:
@@ -34,6 +35,7 @@ def getIssues(page):
         print(title + "写入完毕")
     time.sleep(10)
 
+
 for x in range(page_number):
-    print('爬取第' + str(x) + '页中')
+    print('爬取第' + str(x+1) + '页中...')
     getIssues(x+1)
